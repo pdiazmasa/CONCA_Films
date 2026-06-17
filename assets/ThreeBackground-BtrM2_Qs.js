a2ee0df83b04f77cb405bb5d3f8701356cc7d28a
pdiazmasa <pdiamas1@etsinf.upv.es>
2026-06-17 03:21:07 +0200

    Updates

--- assets/ThreeBackground-BtrM2_Qs.js
+++ assets/ThreeBackground-BtrM2_Qs.js
@@ -1,4 +1,4 @@
-import{r as Ws,j as bo}from"./index-Dn0BS5h-.js";/**
+import{r as Ws,j as bo}from"./index-CFpdTcf0.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
