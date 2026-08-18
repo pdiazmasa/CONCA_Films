46ca5033be4e3bd9044ed59268b5e16f32e436ef
pdiazmasa <pdiamas1@etsinf.upv.es>
2026-08-18 15:44:01 +0200

    Updates

--- assets/ThreeBackground-CtRqibs0.js
+++ assets/ThreeBackground-CtRqibs0.js
@@ -1,4 +1,4 @@
-import{r as Ws,j as bo}from"./index-BXIqAeZM.js";/**
+import{r as Ws,j as bo}from"./index-Y7adUf9t.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
