59df8ca1a201bd548a582dfec0a0287adaf38817
pdiazmasa <pdiamas1@etsinf.upv.es>
2026-08-19 22:13:31 +0200

    Updates

--- assets/ThreeBackground-Bbqx5nsS.js
+++ assets/ThreeBackground-Bbqx5nsS.js
@@ -1,4 +1,4 @@
-import{r as Ws,j as bo}from"./index-MW6Beg_0.js";/**
+import{r as Ws,j as bo}from"./index-DZJkiLU0.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
