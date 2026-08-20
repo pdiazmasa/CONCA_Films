39364d014e4daffcff66b370660dbcd48196e19e
CONCA Films <concafilms@gmail.com>
2026-08-20 00:11:00 +0000

    Hero: titular en mayusculas, mas grande y a pantalla completa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -91,7 +91,7 @@ function Hero() {
           <div style={{ position: 'relative', zIndex: 1 }}>
             <BlurText
               text="Contenido profesional para tu marca"
-              className="text-5xl md:text-6xl lg:text-[5.5rem] font-heading italic font-bold text-white leading-[0.95] max-w-5xl mx-auto tracking-[-2px]"
+              className="text-5xl sm:text-6xl md:text-8xl lg:text-9xl xl:text-[9.5rem] font-heading uppercase font-bold text-white leading-[0.9] max-w-[88vw] lg:max-w-[85vw] mx-auto tracking-[-1px] sm:tracking-[-2px] md:tracking-[-3px] lg:tracking-[-4px]"
               justify="center"
             />
           </div>
